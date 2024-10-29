42601c22293b0866eb1bd1d6c4f0171e36815acb
Hridoy Patwary <rakibulhasan642004@gmail.com>
2024-10-30 05:12:45 +0600

    added reduce button functionality

--- src/js/global.js
+++ src/js/global.js
@@ -7,14 +7,19 @@ const handleLeftMenu = document.querySelector('.handle-left-menu');
 const userBtn = leftMenu.querySelector('.user-btn');
 const themeIcon = changeTheme.querySelector('img');
 
+const welcomeBox = document.querySelector('.welcome-box');
+const wlcmBoxInner = welcomeBox.querySelector('.box-inner');
+const reduceBtn = welcomeBox.querySelector('.reduce');
+
+const boxBoundingRect = wlcmBoxInner.getBoundingClientRect();
+
 
 // all functions and event listeners
 
 changeTheme.addEventListener('click', () => {
     const currentIcon = themeIcon.dataset.currenticon;
 
-
-    if(currentIcon == 'moon'){
+    if (currentIcon == 'moon') {
         themeIcon.src = './assets/sun.svg';
         themeIcon.dataset.currenticon = 'sun';
         documentElm.dataset.theme = 'dark';
@@ -23,7 +28,6 @@ changeTheme.addEventListener('click', () => {
         themeIcon.dataset.currenticon = 'moon';
         documentElm.dataset.theme = 'light';
     }
-
     storeInLocal('theme', documentElm.dataset.theme)
 });
 
@@ -32,6 +36,18 @@ handleLeftMenu.addEventListener('click', () => {
     userBtn.classList.toggle('hide');
     leftMenu.classList.toggle('minimize');
 });
+
+reduceBtn.addEventListener('click', () => {
+    if (reduceBtn.classList.contains('show-more')) {
+        wlcmBoxInner.style.height = boxBoundingRect.height + 'px';
+        reduceBtn.innerHTML = 'Reduce';
+        reduceBtn.classList.remove('show-more');
+    } else {
+        wlcmBoxInner.style.height = (boxBoundingRect.height - 105) + 'px';
+        reduceBtn.innerHTML = 'Show more';
+        reduceBtn.classList.add('show-more');
+    }
+})
 
 
 const storeInLocal = (key, data) => {
@@ -44,14 +60,15 @@ const getFromLocal = (key) => {
 
 window.addEventListener('load', () => {
     const theme = getFromLocal('theme');
-    if(theme){
-        if(theme == 'dark'){
+    if (theme) {
+        if (theme == 'dark') {
             themeIcon.src = './assets/sun.svg';
             themeIcon.dataset.currenticon = 'dark';
-        }else{
+        } else {
             themeIcon.src = './assets/moon.svg';
             themeIcon.dataset.currenticon = 'light';
         }
         documentElm.dataset.theme = theme
     }
-})
+    wlcmBoxInner.style.height = boxBoundingRect.height + 'px';
+});
