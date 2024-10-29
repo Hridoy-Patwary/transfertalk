cf030c99ea70fd4b5ff27790a8208e28c8ac2f6e
Hridoy Patwary <rakibulhasan642004@gmail.com>
2024-10-30 04:56:21 +0600

    added saving theme data using localstorage and file upload UI updated

--- src/js/global.js
+++ src/js/global.js
@@ -1,26 +1,30 @@
+const documentElm = document.documentElement;
+
 const changeTheme = document.querySelector('.change-theme');
 
 const leftMenu = document.querySelector('.left-menu-bar');
 const handleLeftMenu = document.querySelector('.handle-left-menu');
 const userBtn = leftMenu.querySelector('.user-btn');
+const themeIcon = changeTheme.querySelector('img');
 
 
 // all functions and event listeners
 
 changeTheme.addEventListener('click', () => {
-    const themeIcon = changeTheme.querySelector('img');
     const currentIcon = themeIcon.dataset.currenticon;
 
 
     if(currentIcon == 'moon'){
         themeIcon.src = './assets/sun.svg';
         themeIcon.dataset.currenticon = 'sun';
-        document.documentElement.dataset.theme = 'dark';
+        documentElm.dataset.theme = 'dark';
     } else {
         themeIcon.src = './assets/moon.svg';
         themeIcon.dataset.currenticon = 'moon';
-        document.documentElement.dataset.theme = 'light';
+        documentElm.dataset.theme = 'light';
     }
+
+    storeInLocal('theme', documentElm.dataset.theme)
 });
 
 
@@ -28,3 +32,26 @@ handleLeftMenu.addEventListener('click', () => {
     userBtn.classList.toggle('hide');
     leftMenu.classList.toggle('minimize');
 });
+
+
+const storeInLocal = (key, data) => {
+    localStorage.setItem(key, JSON.stringify(data));
+}
+
+const getFromLocal = (key) => {
+    return JSON.parse(localStorage.getItem(key));
+}
+
+window.addEventListener('load', () => {
+    const theme = getFromLocal('theme');
+    if(theme){
+        if(theme == 'dark'){
+            themeIcon.src = './assets/sun.svg';
+            themeIcon.dataset.currenticon = 'dark';
+        }else{
+            themeIcon.src = './assets/moon.svg';
+            themeIcon.dataset.currenticon = 'light';
+        }
+        documentElm.dataset.theme = theme
+    }
+})
