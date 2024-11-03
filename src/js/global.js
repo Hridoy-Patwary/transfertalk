c6abcf619a4d4e746fdc95ff9c1b9b837722166c
Hridoy Patwary <rakibulhasan642004@gmail.com>
2024-11-03 12:44:02 +0600

    added multiple changes to the design and new UI elements added

--- src/js/global.js
+++ src/js/global.js
@@ -3,27 +3,24 @@ const pageLoader = htmlElm.querySelector('.page-loading');
 
 const changeTheme = document.querySelector('.change-theme');
 
+const signInBtnHeader = document.querySelector('header .sign-in.header-right-btn-style');
 const leftMenu = document.querySelector('.left-menu-bar');
 const expandableUserMenu = leftMenu.querySelector('.expandable-user-menu');
 const menuList = leftMenu.querySelectorAll('.menu-container li');
-const innerMenu = expandableUserMenu.querySelector('.inner-menu');
 const handleLeftMenu = document.querySelector('.handle-left-menu');
 const userBtn = leftMenu.querySelector('.user-btn');
 const themeIcon = changeTheme.querySelector('img');
 
 const mainContentArea = document.querySelector('.main-content-area .scroll');
-const welcomeBox = document.querySelector('.welcome-box');
-const wlcmBoxInner = welcomeBox.querySelector('.box-inner');
-const reduceBtn = welcomeBox.querySelector('.reduce');
 
-const boxBoundingRect = wlcmBoxInner.getBoundingClientRect();
 
 const serverUrl = 'http://localhost:4050/';
 
+
 // all functions and event listeners
 
-document.onreadystatechange = function(){
-    if(document.readyState == 'complete'){
+document.onreadystatechange = function () {
+    if (document.readyState == 'complete') {
         pageLoader.classList.add('hide');
     }
 };
@@ -50,31 +47,42 @@ changeTheme.addEventListener('click', () => {
     storeInLocal('theme', htmlElm.dataset.theme)
 });
 
-userBtn.addEventListener('click', () => {
-    leftMenu.classList.toggle('expand-user-menu');
+const handleExpandableUserMenuHeight = (x, z) => {
+    const innerMenu = expandableUserMenu.querySelector('.inner-menu');
     const innerMenuBoundingRect = innerMenu.getBoundingClientRect();
+    let y = 0;
 
-    if(leftMenu.classList.contains('expand-user-menu')){
-        expandableUserMenu.style.height = innerMenuBoundingRect.height + 'px';
-    }else expandableUserMenu.style = '';
-});
+    if (x !== false) {
+        leftMenu.classList.toggle('expand-user-menu');
+    } else y = 69;
+
+    if (z == false) y = -65;
+
+    if (leftMenu.classList.contains('expand-user-menu')) {
+        expandableUserMenu.style.height = innerMenuBoundingRect.height + y + 'px';
+    } else {
+        expandableUserMenu.style = ''
+    };
+}
+
+userBtn.addEventListener('click', handleExpandableUserMenuHeight);
 
 handleLeftMenu.addEventListener('click', () => {
     userBtn.classList.toggle('hide');
     leftMenu.classList.toggle('minimize');
+
+    if (leftMenu.classList.contains('minimize')) {
+        handleExpandableUserMenuHeight(false, true);
+    } else {
+        handleExpandableUserMenuHeight(false, false);
+    }
 });
 
-reduceBtn.addEventListener('click', () => {
-    if (reduceBtn.classList.contains('show-more')) {
-        wlcmBoxInner.style.height = boxBoundingRect.height + 'px';
-        reduceBtn.innerHTML = 'Reduce';
-        reduceBtn.classList.remove('show-more');
-    } else {
-        wlcmBoxInner.style.height = (boxBoundingRect.height - 105) + 'px';
-        reduceBtn.innerHTML = 'Show more';
-        reduceBtn.classList.add('show-more');
-    }
-})
+signInBtnHeader.addEventListener('click', (e) => {
+    e.preventDefault();
+    const signInMenuItem = leftMenu.querySelector(`[data-page="sign-in"]`);
+    signInMenuItem.click();
+});
 
 const storeInLocal = (key, data) => {
     localStorage.setItem(key, JSON.stringify(data));
@@ -84,14 +92,13 @@ const getFromLocal = (key) => {
     return JSON.parse(localStorage.getItem(key));
 }
 
-const loadPageContent = async (pageName, pageSrc) => {
+const loadPageContent = async (pageSrc) => {
     try {
         const response = await fetch(`${pageSrc}.html`);
 
         if (!response.ok) throw new Error("Page not found");
 
         const content = await response.text();
-        window.history.pushState({ pageName }, '', `?pg=${pageName}`);
         return content;
     } catch (error) {
         console.error("Error loading page:", error);
@@ -99,29 +106,20 @@ const loadPageContent = async (pageName, pageSrc) => {
     }
 }
 
-window.addEventListener('load', () => {
-    const theme = getFromLocal('theme');
-    if (theme) {
-        if (theme == 'dark') {
-            themeIcon.src = './assets/sun.svg';
-            themeIcon.dataset.currenticon = 'dark';
-        } else {
-            themeIcon.src = './assets/moon.svg';
-            themeIcon.dataset.currenticon = 'light';
-        }
-        htmlElm.dataset.theme = theme
-    }
-    wlcmBoxInner.style.height = boxBoundingRect.height + 'px';
-});
-
 menuList.forEach((menu) => menu.addEventListener('click', () => {
     const currentUrl = new URL(window.location.href);
     const oldActiveMenu = leftMenu.querySelector('.menu-container li.active');
     const urlStateTxt = menu.innerText.toLowerCase().replaceAll(' ', '-').trim();
+    const page = urlStateTxt;
 
-    currentUrl.searchParams.set('pg', urlStateTxt);
-    window.history.pushState({}, '', currentUrl);
-    oldActiveMenu.classList.remove('active');
+    if (page === 'home') {
+        window.history.replaceState({}, '', window.location.pathname);
+    } else {
+        currentUrl.searchParams.set('pg', urlStateTxt);
+        window.history.pushState({ page }, '', `?pg=${urlStateTxt}`);
+    }
+    
+    if(oldActiveMenu) oldActiveMenu.classList.remove('active');
     menu.classList.add('active');
     updateUIonMenuClick();
 }));
@@ -131,12 +129,10 @@ const updateUIonMenuClick = async () => {
     const oldActiveMenu = leftMenu.querySelector('li.active');
     const currentPageMenu = leftMenu.querySelector(`[data-page=${page}]`);
     const pageSrc = `./pages/${page}`;
-    let pageContent = '';
+    let pageContent = await loadPageContent(pageSrc);
 
-    oldActiveMenu.classList.remove('active');
-    currentPageMenu.classList.add('active');
-    pageContent = await loadPageContent(page, pageSrc);
-
+    if(oldActiveMenu) oldActiveMenu.classList.remove('active');
+    if(currentPageMenu) currentPageMenu.classList.add('active');
     mainContentArea.innerHTML = '';
     mainContentArea.innerHTML = pageContent;
 
@@ -147,18 +143,113 @@ const checkAndUpdateChangedUIeventListeners = () => {
     const fileDragArea = document.querySelector('.drag-and-drop-area');
     const fileUploadInp = document.getElementById('file-upload-inp');
     const createAccSignInPage = document.querySelector('.create-account-sign-in-pg');
+    const signInWithPass = document.querySelector('.login-with-pass');
     const signInSendMeMagicLink = document.querySelector('.send-magic-link');
+    const loginWithPatreon = document.querySelector('.login-with-patreon');
     const faqList = document.querySelectorAll('.faq-page .faq-list .faq-box');
+    const welcomeBox = document.querySelector('.welcome-box');
 
-    if(fileDragArea) handleDragAndDrop(fileDragArea, fileUploadInp);
-    
-    if(signInSendMeMagicLink){
-        signInSendMeMagicLink.addEventListener('click', () => {
+
+    if (fileDragArea) handleDragAndDrop(fileDragArea, fileUploadInp);
+
+    if (signInWithPass) handleSignInWithPass(signInWithPass, signInSendMeMagicLink);
+    if (signInSendMeMagicLink) { handleSendMagicLinkBtn(signInSendMeMagicLink) };
+    if (loginWithPatreon) handleLoginWithPatreonBtn(loginWithPatreon);
+    if(welcomeBox) handleWelcomBox(welcomeBox);
+
+    if (createAccSignInPage) {
+        createAccSignInPage.addEventListener('click', () => {
+            leftMenu.querySelector(`[data-page='create-account']`).click();
+        })
+    }
+
+    if (faqList) handleFaqList(faqList);
+}
+
+const handleWelcomBox = (welcomeBox) => {
+    const wlcmBoxInner = welcomeBox.querySelector('.box-inner');
+    const reduceBtn = welcomeBox.querySelector('.reduce');
+    const boxBoundingRect = wlcmBoxInner.getBoundingClientRect();
+
+    wlcmBoxInner.style.height = boxBoundingRect.height + 'px';
+    reduceBtn.addEventListener('click', () => {
+        if (reduceBtn.classList.contains('show-more')) {
+            wlcmBoxInner.style.height = boxBoundingRect.height + 'px';
+            reduceBtn.innerHTML = 'Reduce';
+            reduceBtn.classList.remove('show-more');
+        } else {
+            wlcmBoxInner.style.height = (boxBoundingRect.height - 105) + 'px';
+            reduceBtn.innerHTML = 'Show more';
+            reduceBtn.classList.add('show-more');
+        }
+    })
+}
+
+const handleLoginWithPatreonBtn = (btn) => {
+    btn.addEventListener('click', () => {
+        console.log('login with patreon btn clicked');
+    })
+}
+
+const handleSignInWithPass = (btn, removeElm) => {
+    const inputs = btn.parentElement.querySelectorAll('input');
+
+    btn.addEventListener('click', () => {
+        if (btn.classList.contains('just-login')) {
+            const emailInp = btn.parentElement.querySelector('.sign-in-email-inp');
+            const passInp = btn.parentElement.querySelector('.sign-in-pass');
+            let checkEmptyInp = false;
+
+            inputs.forEach((inp) => {
+                if (inp.value === '') {
+                    checkEmptyInp = true;
+                    inp.style.borderColor = 'rgb(207, 8, 8)';
+                } else inp.style = '';
+            });
+
+            if (checkEmptyInp === false) {
+                const data = {
+                    email: emailInp,
+                    pass: passInp
+                }
+
+                console.log('send data to server');
+                fetch(serverUrl + 'api/v1/sign-in', {
+                    method: 'post',
+                    body: JSON.stringify(data)
+                }).then(res => res.json()).then((data) => {
+                    console.log(data);
+                }).catch((err) => {
+                    console.log(err);
+                })
+            }
+        } else {
+            const forgotPassElm = document.createElement('button');
+            const inpPassContainer = document.querySelector('.input-pass-container');
+
+            btn.innerHTML = 'Login'
+            btn.classList.add('just-login');
+            forgotPassElm.innerHTML = 'I forgot my password'
+            forgotPassElm.className = 'forgot-pass';
+            btn.parentElement.append(forgotPassElm)
+            inpPassContainer.classList.add('show');
+            removeElm.remove();
+        }
+    })
+}
+
+const handleSendMagicLinkBtn = (btn) => {
+    const emailInp = btn.parentElement.querySelector('input.sign-in-email-inp');
+
+    btn.addEventListener('click', () => {
+        if (emailInp.value === '') {
+            emailInp.style.borderColor = 'rgb(207, 8, 8)';
+        } else {
             const data = {
-                testing: true,
+                email: emailInp.value,
                 data: false
             }
-            fetch(serverUrl+'api/v1/sign-in', ({
+            fetch(serverUrl + 'api/v1/sign-in', ({
                 method: 'post',
                 headers: {
                     'Content-Type': 'application/json',
@@ -168,94 +259,9 @@ const checkAndUpdateChangedUIeventListeners = () => {
                 console.log(data);
             }).catch(err => {
                 console.log(err);
-            })
-        });
-    }
-    if(createAccSignInPage) {
-        createAccSignInPage.addEventListener('click', () => {
-            leftMenu.querySelector(`[data-page='create-account']`).click();
-        })
-    }
-
-    if(faqList) handleFaqList(faqList);
-}
-
-const handleDragAndDrop = (dragArea, fileInp) => {
-    const dragAreaContent = dragArea.querySelector('.content');
-
-    dragArea.addEventListener('click', (e) => {
-        const target = e.target;
-        if(target.className !== 'validate-and-upload'){
-            fileInp.click()
+            });
+            emailInp.style = ''
         }
-    })
-
-    dragArea.addEventListener('dragover', (e) => {
-        e.stopPropagation();
-        e.preventDefault();
-        e.dataTransfer.dropEffect = 'copy';
-        dragArea.classList.add('dragging-over');
-    });
-
-    dragArea.addEventListener('dragleave', (e) => {
-        e.stopPropagation();
-        e.preventDefault();
-        dragArea.classList.remove('dragging-over');
-    });
-    
-    dragArea.addEventListener('drop', (e) => {
-        e.stopPropagation();
-        e.preventDefault();
-        const droppedFiles = e.dataTransfer.files;
-
-        if(droppedFiles.length > 0){
-            sendFilesToServer(droppedFiles, dragAreaContent);
-        }else console.log('No file chosen');
-        dragArea.classList.remove('dragging-over');
-    });
-
-    fileInp.addEventListener('change', () => {
-        const files = fileInp.files;
-        sendFilesToServer(files, dragAreaContent);
-    });
-}
-
-const sendFilesToServer = (fileList, contentArea) => {
-    const contentAreaTitle = contentArea.querySelector('p');
-    const addFilesBtn = contentArea.querySelector('button');
-    const addMoreOrSendContainer = document.createElement('div');
-    const filesOrFile = fileList.length == 1 ? 'file' : 'files';
-
-    addMoreOrSendContainer.innerHTML = `<button class="add-more-files">Add more files</button>
-                                        <button class="validate-and-upload" type="button">Validate and upload</button>`;
-
-    addFilesBtn.remove();
-    addMoreOrSendContainer.className = 'add-more-or-send';
-    contentArea.append(addMoreOrSendContainer);
-    contentAreaTitle.innerHTML = fileList.length + ` selected ${filesOrFile} ready to send`;
-
-    // validate and upload to server
-    const validateAndUploadBtn = contentArea.querySelector('.validate-and-upload');
-    validateAndUploadBtn.addEventListener('click', (e) => {
-        e.preventDefault();
-        const formData = new FormData();
-
-        for (let i = 0; i < fileList.length; i++) {
-            const file = fileList[i];
-            formData.append('files', file);
-        }
-        
-        fetch(serverUrl+'api/v1/upload', {
-            method: 'POST',
-            body: formData
-        }).then(res => {
-            if (!res.ok) throw new Error('Network response was not ok');
-            return res.json();
-        }).then((data) => {
-            console.log(data);
-        }).catch((err) => {
-            console.log('Fetch error:', err);
-        });
     });
 }
 
@@ -268,13 +274,33 @@ const handleFaqList = (faqElmList) => {
         faq.addEventListener('click', () => {
             const expandHeight = faqContentBoudingRect.height + faqBoxBoudingRect.height + 20;
             faq.classList.toggle('expanded');
-            if(faq.classList.contains('expanded')){
+            if (faq.classList.contains('expanded')) {
                 faq.style.height = faqBoxBoudingRect.height + 'px';
-            }else{
+            } else {
                 faq.style.height = expandHeight + 1 + 'px';
             }
         });
     })
 }
 
+
 updateUIonMenuClick();
+
+window.addEventListener('popstate', updateUIonMenuClick);
+
+window.addEventListener('load', () => {
+    const theme = getFromLocal('theme');
+
+    if (theme) {
+        if (theme == 'dark') {
+            themeIcon.src = './assets/sun.svg';
+            themeIcon.dataset.currenticon = 'dark';
+        } else {
+            themeIcon.src = './assets/moon.svg';
+            themeIcon.dataset.currenticon = 'light';
+        }
+        htmlElm.dataset.theme = theme
+    }
+});
+
+
